d34a5d9c3e5e38ae841d565e5e864ff546f58b28
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-03-24 23:17:10 -0300

    Improve timer responsibility

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -1,30 +1,23 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import {
   decrementTime,
 } from './timerSlice';
-
-function calcTimer(timeRemaining) {
-  const timer = {
-    minutes: Math.floor(timeRemaining / 60),
-    seconds: timeRemaining % 60,
-  }
-
-  return timer;
-}
 
 export function Timer() {
   const timeRemaining = useSelector((state) => state.timer.timeRemaining);
   const isRunning = useSelector((state) => state.timer.isRunning);
   const dispatch = useDispatch();
 
-  const [timer, setTimer] = useState(calcTimer(timeRemaining));
+  const timer = {
+    minutes: Math.floor(timeRemaining / 60),
+    seconds: timeRemaining % 60,
+  }
 
   useEffect(() => {
     const timeout = setTimeout(() => {
       if (isRunning) {
         dispatch(decrementTime());
-        setTimer(calcTimer(timeRemaining));
       }
     }, 1000);
 

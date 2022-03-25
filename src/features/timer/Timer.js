6e5265ec19ee3f3f6b5853c65aa5aacbc46128ef
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-03-25 15:49:38 -0300

    Fix scss selector

--- src/features/timer/Timer.js
+++ src/features/timer/Timer.js
@@ -27,7 +27,7 @@ export function Timer() {
   });
 
   return (
-    <div onClick={() => dispatch(switchIsRunning())}>
+    <div className="Timer" onClick={() => dispatch(switchIsRunning())}>
       <span>
         {timer.minutes.toString().padStart(2, '0')}
       </span>

d13cf9b33960c6aeb732fed8810b72543bc3875f
Jheam Storch Ross <jheam.storch@hotmail.com>
2022-03-25 21:29:42 -0300

    Set up FontAwesome icons

--- src/features/numberPicker/NumberPicker.js
+++ src/features/numberPicker/NumberPicker.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { solid } from '@fortawesome/fontawesome-svg-core/import.macro';
 
 export function NumberPicker(props) {
   return (
     <div className='NumberPicker'>
       <button onClick={() => props.onCounterChange(props.counter - (+props.changeAmount))}>
-        --
+        <FontAwesomeIcon icon={solid('minus')} />
       </button>
       <span>{props.counter}</span>
       <button onClick={() => props.onCounterChange(props.counter + (+props.changeAmount))}>
-        ++
+        <FontAwesomeIcon icon={solid('plus')} />
       </button>
     </div>
   );
